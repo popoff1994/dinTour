f5b7ff2582434491019bc1ae551597e98be3bb06
antonharder1 <anto1388@edu.zealand.dk>
2022-05-17 18:00:30 +0200

    Made InfoPage Yuh 😫🥱👌🏾🤙🏾✌🏾

---
+++ dinTour/Pages/Forside.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace dinTour.Pages
+{
+    public class ForsideModel : PageModel
+    {
+        public void OnGet()
+        {
+            //OnGet? More like get some bitches, virgin 😎😎.
+        }
+    }
+}
