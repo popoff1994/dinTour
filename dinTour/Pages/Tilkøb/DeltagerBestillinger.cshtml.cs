fab7949b00e4408c4da0a87b308be42ffe9d796a
popoff1994 <jonasmadasenfake@gmail.com>
2022-05-23 12:41:38 +0200

    Update

--- dinTour/Pages/Tilkøb/DeltagerBestillinger.cshtml.cs
+++ dinTour/Pages/Tilkøb/DeltagerBestillinger.cshtml.cs
@@ -2,35 +2,35 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using dinTour.Models;
 using dinTour.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace dinTour.Pages.Parkering
+namespace dinTour.Pages.Tilkøb
 {
-    public class ParkeringDeltagerModel : PageModel
+    public class DeltagerBestillingerModel : PageModel
     {
 
         public DeltagerService DeltagerService { get; set; }
         public BookningService BookningService { get; set; }
+        public TilkøbService TilkøbService { get; set; }
 
 
         //public IEnumerable<OrderDAO> MyOrders { get; set; }
         public IEnumerable<Models.Bookning> CurrentBookings { get; set; }
+        public IEnumerable<Models.Tilkøb> CurrentTilkøb { get; set; }
 
-        public ParkeringDeltagerModel(DeltagerService deltagerService, BookningService bookningService)
+        public DeltagerBestillingerModel(DeltagerService deltagerService, BookningService bookningService, TilkøbService tilkøbService)
         {
             DeltagerService = deltagerService;
             BookningService = bookningService;
+            TilkøbService = tilkøbService;
 
         }
         public async Task<IActionResult> OnGetAsync()
         {
             CurrentBookings = BookningService.BookningList.ToList();
-            //Deltager CurrentUser = DeltagerService.GetUserByUserName(HttpContext.User.Identity.Name);
-            //MyOrders = UserService.GetUserOrders(CurrentUser);
-            //CurrentBookings = DeltagerService.GetParkingByUser(CurrentUser).Result.Bookning;
+            CurrentTilkøb = TilkøbService.TilkøbsList.ToList();
             return Page();
         }
     }
