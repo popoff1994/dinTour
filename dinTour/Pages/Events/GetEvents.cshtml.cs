ac53383351392f2fddab83987c43915b1993b338
Jonas Madsen <jona586e@easjdk.onmicrosoft.com>
2022-06-13 11:59:12 +0200

    Update - EventTilmeld

--- dinTour/Pages/Events/GetEvents.cshtml.cs
+++ dinTour/Pages/Events/GetEvents.cshtml.cs
@@ -14,18 +14,22 @@ namespace dinTour.Pages.Events
     public class GetEventsModel : PageModel
     {
         public List<Begivenhed> Begivenheder { get; set; }
-        
+
+        public List<Deltager> Deltagere { get; set; }
+        public DeltagerService DeltagerService { get; set; }
 
         public BegivenhedService _begivenhedService;
 
-        public GetEventsModel(BegivenhedService begivenhedService)
+        public GetEventsModel(BegivenhedService begivenhedService, DeltagerService deltagerService)
         {
             this._begivenhedService = begivenhedService;
+            this.DeltagerService = deltagerService;
         }
 
 
         public async Task<IActionResult> OnGetAsync()
         {
+            Deltagere = DeltagerService.GetUsers();
             Begivenheder = _begivenhedService.GetAllBegivenheder().ToList();
             return Page();
         }
